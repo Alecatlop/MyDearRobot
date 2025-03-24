179b44d66febe3902e2ed8803178f03ac7f3ca33
Alecatlop <alecatlop@alu.edu.gva.es>
2025-03-24 21:22:31 +0100

    prueba Sliders audio

--- Assets/Scripts/Interfaz/CanvasManager.cs
+++ Assets/Scripts/Interfaz/CanvasManager.cs
@@ -12,12 +12,14 @@ public class CanvasManager : MonoBehaviour
     GameObject opciones;
 
     GameObject sliderbrillo;
-    float numbrillo;
     public Image panelbrillo;
 
     GameObject sliderefectos;
-    float numefectos;
-    
+    GameObject slidermusica;
+
+    public AudioSource boton;
+    public AudioSource musica;
+
 
     // Start is called before the first frame update
     void Start()
@@ -27,10 +29,13 @@ public class CanvasManager : MonoBehaviour
 
         sliderbrillo = GameObject.Find("SliderBrillo");
         sliderefectos = GameObject.Find("SliderEfectos");
-        
+        slidermusica = GameObject.Find("SliderMusica");
 
         configuracion.SetActive(false);
         opciones.SetActive(true);
+
+        slidermusica.GetComponent<Slider>().value = PlayerPrefs.GetFloat("musica", 0.5f);
+        sliderefectos.GetComponent<Slider>().value = PlayerPrefs.GetFloat("efectos", 0.5f);
 
         sliderbrillo.GetComponent<Slider>().value = PlayerPrefs.GetFloat("brillo", 0f);
         panelbrillo.color = new Color(panelbrillo.color.r, panelbrillo.color.g, panelbrillo.color.b, sliderbrillo.GetComponent<Slider>().value);
@@ -43,37 +48,45 @@ public class CanvasManager : MonoBehaviour
 
     public void Jugar()
     {
+        boton.Play();
         SceneManager.LoadScene("Catret");
     }
 
     public void Configuracion()
     {
+        boton.Play();
         configuracion.SetActive(true);
         opciones.SetActive(false);
     }
 
     public void Brillo(float valor)
     {
-        numbrillo = valor;
         PlayerPrefs.GetFloat("brillo", sliderbrillo.GetComponent<Slider>().value);
         panelbrillo.color = new Color(panelbrillo.color.r, panelbrillo.color.g, panelbrillo.color.b, sliderbrillo.GetComponent<Slider>().value);
     }
 
     public void Efectos(float valor)
     {
-        numefectos = valor;
-        PlayerPrefs.GetFloat("efectos", numefectos);
-        AudioListener.volume = sliderefectos.GetComponent<Slider>().value;
+        PlayerPrefs.GetFloat("efectos", sliderefectos.GetComponent<Slider>().value);
+        boton.volume = sliderefectos.GetComponent<Slider>().value;
+    }
+
+    public void Musica(float valor)
+    {
+        PlayerPrefs.GetFloat("musica", slidermusica.GetComponent<Slider>().value);
+        musica.volume = slidermusica.GetComponent<Slider>().value;
     }
 
     public void Regresar()
     {
+        boton.Play();
         configuracion.SetActive(false);
         opciones.SetActive(true);
     }
 
     public void Salir()
     {
+        boton.Play();
         Application.Quit();
     }
 }
