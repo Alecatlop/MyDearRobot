b760f5b974ee629fc0e1aac98910fc564d3ff639
Alecatlop <152001029+Alecatlop@users.noreply.github.com>
2025-04-04 09:42:07 +0200

    Correccion spawn referencia
    Sori ve a Terapia

--- Assets/Scripts/Personaje/Spawn.cs
+++ Assets/Scripts/Personaje/Spawn.cs
@@ -11,7 +11,7 @@ public class Spawn : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        player = GetComponent<CharacterControllerScript>();
+        
     }
 
     // Update is called once per frame
