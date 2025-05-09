d1bb0db0c4362c719b6b3b903d095927fb506a32
Soriniel <soriniel02@hotmail.com>
2025-05-09 09:26:55 +0200

    Animación baldosa caida

--- Assets/Scripts/Niveles/NIvel3/Baldosa.cs
+++ Assets/Scripts/Niveles/NIvel3/Baldosa.cs
@@ -6,39 +6,47 @@ public class Baldosa : MonoBehaviour
 {
     public bool correcto;
     public Renderer runarenderer;
-    
+    float velocidadCaida = 10f;
 
-    // Start is called before the first frame update
+    private bool caer = false;
+    private Vector3 posicionObjetivo;
+
     void Start()
     {
+        // Inicialmente no hay posición objetivo
+        posicionObjetivo = transform.position;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (caer)
+        {
+            // Mover suavemente hacia la posición objetivo
+            transform.position = Vector3.MoveTowards(transform.position, posicionObjetivo, velocidadCaida * Time.deltaTime);
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.name == "Jugador")
         {
+            Material material = runarenderer.material;
 
-            if (correcto == true)
+            if (correcto)
             {
                 Debug.Log("hola");
-                Material material = runarenderer.material;
-                material.SetColor("_EmissionColor", Color.green);  // Establece el color de emisi�n
-                material.EnableKeyword("_EMISSION");  // Aseg�rate de que la emisi�n est� activada
-            }
-
-            else
-            {
-                Material material = runarenderer.material;
-                material.SetColor("_EmissionColor", Color.red);  // Establece el color de emisi�n a rojo
+                material.SetColor("_EmissionColor", Color.green);
                 material.EnableKeyword("_EMISSION");
             }
+            else
+            {
+                material.SetColor("_EmissionColor", Color.red);
+                material.EnableKeyword("_EMISSION");
 
+                // Establece la nueva posición objetivo más abajo
+                posicionObjetivo = transform.position + Vector3.down * 100f; // Puedes ajustar la distancia
+                caer = true;
+            }
         }
     }
 }
